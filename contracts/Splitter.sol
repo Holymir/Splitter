e6138865b2a3ea094c4f4f106e9fa34aab2b8271
holymir <vtsochev@abv.bg>
2018-03-09 13:45:21 +0200

    events and tests for them added

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -3,23 +3,33 @@ pragma solidity ^0.4.19;
 contract Splitter {
     
     address public owner;
-    mapping (address => uint) public participants;    
+
+    // Balances for each account
+    mapping (address => uint) public balances;    
     
+	event WhenDeposit(address _from, address _toAddr1, address _toAddr2, uint _value); 
+	event WhenWithDraw(address _from, uint _value);
+	event WhenKilled(address _addr);
+
+
     function Splitter() public{
         owner = msg.sender;
     }
 
+    // Check is the address valid
 	modifier onlyAddressValid(address _addr) { 
 		require(_addr != address(0)); 
 		_; 
 	}
 
+	// Check if the function is called by owner
 	modifier onlyOwner() { 
 		require(msg.sender == owner); 
 		_; 
 	}
 	
 
+	// Recieves funds from owner and split them between two address 
 	function split(address _addr1, address _addr2) public payable 
 		onlyAddressValid(_addr1) 
 		onlyAddressValid(_addr2)
@@ -29,23 +39,28 @@ contract Splitter {
 		require(owner != _addr1 && owner != _addr2);
 
 		uint amount = msg.value;
-		participants[owner] += amount % 2;
+		balances[owner] += amount % 2;
         amount /= 2;
 
-        participants[_addr1] += amount;
-        participants[_addr2] += amount;		
+        balances[_addr1] += amount;
+        balances[_addr2] += amount;	
+
+        WhenDeposit(msg.sender, _addr1, _addr2, amount);	
 	}
 
+	// Recipients withDraw their coins
 	function withDraw() public {
 		
-		uint amountToWithDraw = participants[msg.sender];
+		uint amountToWithDraw = balances[msg.sender];
 		require(amountToWithDraw > 0);
-		participants[msg.sender] = 0;		
-		msg.sender.transfer(amountToWithDraw);		
+		balances[msg.sender] = 0;		
+		msg.sender.transfer(amountToWithDraw);	
+		WhenWithDraw(msg.sender, amountToWithDraw);	
 	}
 	
-	
+	// Removes the owner
 	function kill() public onlyOwner(){
+	    WhenKilled(owner);
 	    selfdestruct(this);
 	}
 }
