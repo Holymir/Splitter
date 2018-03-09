48de8493f483858f6a4cf13140ee304df8693a59
holymir <vtsochev@abv.bg>
2018-03-09 11:06:44 +0200

    added mapping to hold the recipients fund

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -3,28 +3,58 @@ pragma solidity ^0.4.19;
 contract Splitter {
     
     address public owner;
+    mapping (address => uint) public participants;    
     
     function Splitter() public{
         owner = msg.sender;
     }
 
-	modifier isAddressValid(address _addr1, address _addr2) { 
-		require(_addr1 != 0x0000000000000000000000000000000000000000); 
-		require(_addr2 != 0x0000000000000000000000000000000000000000); 
+	modifier isAddressValid(address _addr) { 
+		require(_addr != address(0)); 
 		_; 
 	}
 
-	function split(address _addr1, address _addr2) public payable isAddressValid(_addr1, _addr2) {
-		require(msg.sender.balance > msg.value);
-		require(msg.sender != _addr1 && msg.sender != _addr2);
-        uint amount = msg.value / 2;
-		_addr1.transfer(amount);
-		_addr2.transfer(amount);
+	modifier isOwner(address _ownerAddr) { 
+		require(_ownerAddr == owner); 
+		_; 
 	}
 	
-	function kill() public returns(bool){
+
+	function split(address _addr1, address _addr2) public payable 
+		isAddressValid(_addr1) 
+		isAddressValid(_addr2)
+		isOwner(msg.sender) {
+
+		require(msg.value > 0);
+		require(owner != _addr1 && owner != _addr2);
+
+		uint amount = msg.value;
+
+		if(amount % 2 == 1) {
+			amount -= 1;
+			participants[owner] += 1;
+		}
+
+        amount /= 2;
+
+        participants[_addr1] += amount;
+        participants[_addr2] += amount;		
+	}
+
+	function withDraw() public {
+
+		// msg.sender.transfer(participants[msg.sender]);
+		// participants[msg.sender] = 0;
+		address sender = msg.sender;
+		uint amountToWithDraw = participants[sender];
+		require(amountToWithDraw > 0);
+		participants[sender] = 0;		
+		sender.transfer(amountToWithDraw);		
+	}
+	
+	
+	function kill() public{
 	    require(owner == msg.sender);
-	    selfdestruct(this);
-	    return true;
+	    selfdestruct(owner);
 	}
 }
--- test/SplitterCheckFunctions.js
+++ test/SplitterCheckFunctions.js
@@ -4,27 +4,28 @@ contract('Splitter', function(accounts) {
 
 	var contract;
 
-	var owner;
-	var accOne;	
-	var accTwo;
+	const owner = accounts[0];
+	const accOne = accounts[1];	
+	const accTwo = accounts[2];
+	const accThree = accounts[3];
 
-	var ballance1;
-	var ballance2;
-	var ownerBallance;
+	var Ballanceowner;
+	var ballanceAccOne;
+	var ballanceAccTwo;
+	var ballanceAccThree;
+
+	var contractBallanceOwner;
+	var contractBallanceAccOne;
+	var contractBallanceAccTwo;
+	var contractBallanceAccThree;
 
 	beforeEach(async () => {
 
-		contract = await Splitter.new();
-
-		owner = accounts[0];
-		accOne = accounts[1];
-		accTwo = accounts[2];
-
-		ownerBallance = web3.eth.getBalance(owner).toNumber();
-		ballanceAccOne = web3.eth.getBalance(accOne).toNumber();
-		ballanceAccTwo = web3.eth.getBalance(accTwo).toNumber();
+		contract = await Splitter.new();		
 		
 	});	
+
+	//Checking owner
 
 	it('has an correct owner', async function () {
 
@@ -32,31 +33,86 @@ contract('Splitter', function(accounts) {
 
     })
 
-	it("it should split corectly from owner to acc1", async function() {
+    //Checking Split function
+
+	it("it should split corectly from owner to accOne and accTwo when value is even", async function() {
+
+		contractBallanceAccOne = await contract.participants(accOne);
+		contractBallanceAccTwo = await contract.participants(accTwo);
 
 		await contract.split(accOne, accTwo, {from: owner, value: 500});
 
-		var newBallanceAccOne = web3.eth.getBalance(accOne).toNumber();
+		let newContractBallanceAccOne = await contract.participants(accOne);
+		let newContractBallanceAccTwo = await contract.participants(accTwo);
 
-		assert.strictEqual(newBallanceAccOne, ballanceAccOne + 250, "doesn't split corectly from owner to acc1");				
+		assert.strictEqual(newContractBallanceAccOne.toNumber(), contractBallanceAccOne.toNumber() + 250, "doesn't split corectly from owner to accOne");
+		assert.strictEqual(newContractBallanceAccTwo.toNumber(), contractBallanceAccTwo.toNumber() + 250, "doesn't split corectly from owner to accTwo");				
 	});	
 
-	it("it should split corectly from owner to acc2", async function() {
+	it("it should split corectly from owner to accOne and accTwo when value is odd", async function() {
 
-		await contract.split(accOne, accTwo, {from: owner, value: 500});
+		contractBallanceOwner = await contract.participants(owner);
+		contractBallanceAccOne = await contract.participants(accOne);
+		contractBallanceAccTwo = await contract.participants(accTwo);
 
-		var newBallanceAccTwo = web3.eth.getBalance(accTwo).toNumber();
+		await contract.split(accOne, accTwo, {from: owner, value: 501});
 
-		assert.strictEqual(newBallanceAccTwo, ballanceAccTwo + 250, "doesn't split corectly from owner to acc2");				
+		let newContractBallanceOwner = await contract.participants(owner);
+		let newContractBallanceAccOne = await contract.participants(accOne);
+		let newContractBallanceAccTwo = await contract.participants(accTwo);
+
+		assert.strictEqual(newContractBallanceOwner.toNumber(), contractBallanceOwner.toNumber() + 1, "doesn't split corectly to Owner");
+		assert.strictEqual(newContractBallanceAccOne.toNumber(), contractBallanceAccOne.toNumber() + 250, "doesn't split corectly from owner to accOne");
+		assert.strictEqual(newContractBallanceAccTwo.toNumber(), contractBallanceAccTwo.toNumber() + 250, "doesn't split corectly from owner to accTwo");				
 	});
 
-	it("it should split corectly from another acc to owner", async function() {
+	it("it should split corectly from owner to accOne and accTwo twice in a row", async function() {
 
-		await contract.split(owner, accTwo, {from: accOne, value: 500});
+		contractBallanceAccOne = await contract.participants(accOne);
+		contractBallanceAccTwo = await contract.participants(accTwo);
 
-		var newBallanceAccOwner = web3.eth.getBalance(owner).toNumber();
+		await contract.split(accOne, accTwo, {from: owner, value: 500});
+		await contract.split(accOne, accTwo, {from: owner, value: 200});
 
-		assert.strictEqual(newBallanceAccOwner, ownerBallance + 250, "doesn't split corectly from another acc to owner");				
+		let newContractBallanceAccOne = await contract.participants(accOne);
+		let newContractBallanceAccTwo = await contract.participants(accTwo);
+
+		assert.strictEqual(newContractBallanceAccOne.toNumber(), contractBallanceAccOne.toNumber() + 350, "doesn't split corectly from owner to accOne");
+		assert.strictEqual(newContractBallanceAccTwo.toNumber(), contractBallanceAccTwo.toNumber() + 350, "doesn't split corectly from owner to accTwo");				
 	});	
 
+	it("it should split corectly two transactions between three participants", async function() {
+
+		contractBallanceAccOne = await contract.participants(accOne);
+		contractBallanceAccTwo = await contract.participants(accTwo);
+		contractBallanceAccThree = await contract.participants(accThree);
+
+		await contract.split(accOne, accTwo, {from: owner, value: 300});
+		await contract.split(accOne, accThree, {from: owner, value: 260});
+
+		let newContractBallanceAccOne = await contract.participants(accOne);
+		let newContractBallanceAccTwo = await contract.participants(accTwo);
+		let newContractBallanceAccThree = await contract.participants(accThree);
+
+
+		assert.strictEqual(newContractBallanceAccOne.toNumber(), contractBallanceAccOne.toNumber() + 280, "doesn't split corectly from owner to accOne");
+		assert.strictEqual(newContractBallanceAccTwo.toNumber(), contractBallanceAccTwo.toNumber() + 150, "doesn't split corectly from owner to accTwo");
+		assert.strictEqual(newContractBallanceAccThree.toNumber(), contractBallanceAccThree.toNumber() + 130, "doesn't split corectly from owner to accThree");				
+	});
+
+	//Checking WithDraw
+
+	it("it should withdraw corectly from contract to accOne", async function() {
+
+		await contract.split(accOne, accTwo, {from: owner, value: 500});
+		contractBallanceAccOne = await contract.participants(accOne);		
+		//console.log(contractBallanceAccOne.toNumber())
+		assert.strictEqual(contractBallanceAccOne.toNumber(), 250, "doesn't withdraw correct");
+
+		await contract.withDraw({from: accOne});
+		let newContractBallanceAccOne = await contract.participants(accOne);
+		//console.log(newContractBallanceAccOne.toNumber())		
+
+		assert.strictEqual(newContractBallanceAccOne.toNumber(), 0, "doesn't withdraw correct");
+	});
 });
